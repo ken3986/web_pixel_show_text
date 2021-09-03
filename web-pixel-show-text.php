a827340418e09dac425019b5144687513c949a31
ken3986 <naka_ken8686@yahoo.co.jp>
2021-09-03 21:20:21 +0900

    更新確認機能を実装

--- web-pixel-show-text.php
+++ web-pixel-show-text.php
@@ -8,5 +8,17 @@ Author URI: https://www.webopixel.net/wordpress/631.html
 Version: 0.1
 */
 
+function web_pixel_show_text() {
+  echo 'おはようございます。';
+}
+web_pixel_show_text();
 
+// 更新の確認
+require 'lib/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/ken3986/web_pixel_show_text',
+  __FILE__,
+  'web-pixel-show-text'
+);
+$myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
 ?>
