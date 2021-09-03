30f9edac9ee11f29c879885379aa8b6f8c8c2b70
ken3986 <naka_ken8686@yahoo.co.jp>
2021-09-03 17:55:10 +0900

    initial commit

---
+++ web-pixel-show-text.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Web Pixel Show Text
+Plugin URI: https://www.webopixel.net/wordpress/631.html
+Description: テキストを表示するだけのプラグイン
+Author:
+Author URI: https://www.webopixel.net/wordpress/631.html
+Version: 0.1
+*/
+
+
+?>
